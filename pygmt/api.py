2f7e2ac1f9803a2d12d2d466ce8705460c986345
ian-r-rose <ian.r.rose@gmail.com>
2013-10-28 11:26:47 -0700

    Move some types into gmt_types

--- pygmt/api.py
+++ pygmt/api.py
@@ -1,28 +1,11 @@
 from flags import *
 import ctypes
+from gmt_types import GMT_Pointer, GMT_Error
 
 # import the GMT 5 library.  Must be a shared library.
 # Should have a better way of locating it later
 libgmt = ctypes.CDLL("libgmt.so")
 
-class GMT_Pointer(ctypes.c_void_p):
-    '''
-    Unusual construct, but this is a type for storing the
-    gmt session pointer.  It is exactly the same as the c_void_p
-    type, which is just the normal void* in C.  We want to subclass
-    this because the Python automatically converts the return
-    value of void* to long int.  We'd rather avoid this casting
-    and keep it as a c_void_p, and this subclassing accomplishes that.
-    Also, kind of nice to work with richer types.
-    '''
-    pass
-
-
-class GMT_Error(Exception):
-    '''
-    Class for a simple GMT error message. Does very little
-    '''
-    pass
 
 class GMT_Session:
     '''
--- pygmt/gmt_types.py
+++ pygmt/gmt_types.py
@@ -2,8 +2,31 @@ import ctypes
 import numpy as np
 import _gmt_vector
 
+class GMT_Pointer(ctypes.c_void_p):
+    '''
+    Unusual construct, but this is a type for storing the
+    gmt session pointer.  It is exactly the same as the c_void_p
+    type, which is just the normal void* in C.  We want to subclass
+    this because the Python automatically converts the return
+    value of void* to long int.  We'd rather avoid this casting
+    and keep it as a c_void_p, and this subclassing accomplishes that.
+    Also, kind of nice to work with richer types.
+    '''
+    pass
+
+
+class GMT_Error(Exception):
+    '''
+    Class for a simple GMT error message. Does very little
+    '''
+    pass
 
 class GMT_Vector:
+    '''
+    Class for storing GMT_VECTOR objects.  In the initializer
+    you give it a list of 1D numpy arrays.  It then constructs
+    a GMT_VECTOR which GMT knows how to read.
+    '''
     def __init__(self, arrays):
         self.ptr = ctypes.c_void_p(_gmt_vector.gmt_vector_from_array_list(arrays))
 
