0da70861a102a7b017a221361789415386b22e7f
ian-r-rose <ian.r.rose@gmail.com>
2013-10-29 13:47:47 -0700

    Missed some spots

--- tests/grd_test.py
+++ tests/grd_test.py
@@ -21,7 +21,7 @@ lons = lons*180.0/np.pi
 lats = 90.0-lats*180.0/np.pi
 
 #recover the spherical harmonic with contouring
-fig = pygmt.GMT_Figure("output.ps", range='g', projection='G-75/41/7i', verbose=False)
+fig = pygmt.GMT_Figure("output.ps", figure_range='g', projection='G-75/41/7i', verbose=False)
 grid = fig.surface('-I5/5 -Rg', pygmt.GMT_Vector([lons,lats,vals]))
 fig.grd2cpt('-Chot', grid, 'pal.cpt')
 fig.grdimage('-Cpal.cpt -E100i', grid)
