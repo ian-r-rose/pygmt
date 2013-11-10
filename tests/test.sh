9afa3b93ce2d1611ef81d433306114d549420bbf
ian-r-rose <ian.r.rose@gmail.com>
2013-11-09 19:37:28 -0800

    Move test.sh into the tests directory

--- tests/test.sh
+++ tests/test.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-cd tests
 for test in `ls *.py`
 do
     echo "Running test $test"
     python $test &> $test.tmp || { echo "Test $test failed"; exit 1; }
     rm $test.tmp
 done
-cd ..
-
 
